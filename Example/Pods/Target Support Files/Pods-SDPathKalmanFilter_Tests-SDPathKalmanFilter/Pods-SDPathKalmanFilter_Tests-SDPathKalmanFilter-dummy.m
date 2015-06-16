90c356c957898388cd7bb60f1cdd0e0402cf594b
Marek Matulski <marek@swingdev.io>
2015-06-16 16:21:03 +0200

    Pods temp dir removed

--- Example/Pods/Target Support Files/Pods-SDPathKalmanFilter_Tests-SDPathKalmanFilter/Pods-SDPathKalmanFilter_Tests-SDPathKalmanFilter-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SDPathKalmanFilter_Tests_SDPathKalmanFilter : NSObject
-@end
-@implementation PodsDummy_Pods_SDPathKalmanFilter_Tests_SDPathKalmanFilter
-@end
--- Example/Pods/Target Support Files/Pods-SDPathKalmanFilter_Tests-SDPathKalmanFilter/Pods-SDPathKalmanFilter_Tests-SDPathKalmanFilter-umbrella.h
+++
@@ -1,9 +0,0 @@
-#import <UIKit/UIKit.h>
-
-#import "SDKalmanFilter.h"
-#import "SDMatrix.h"
-#import "SDPathKalmanFilter.h"
-
-FOUNDATION_EXPORT double SDPathKalmanFilterVersionNumber;
-FOUNDATION_EXPORT const unsigned char SDPathKalmanFilterVersionString[];
-
